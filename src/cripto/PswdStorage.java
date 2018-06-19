6c2e5dbeb4c20fc9f5e58a3f7953991cd15bc5da
digodk <diogodklein@gmail.com>
2018-06-19 19:59:07 -0300

    Criadas as funções de hash

--- src/cripto/PswdStorage.java
+++ src/cripto/PswdStorage.java
@@ -1,5 +1,13 @@
 package cripto;
 
 public class PswdStorage {
-
+  public String clientPswdHash(String pswd, String salt) {
+    String hash;
+    hash = "";
+    return hash;
+  }
+  
+  public String clientPswdHash(String Paciente) {
+    return clientPswdHash("Senha", "Nome Usuário");
+  }
 }
