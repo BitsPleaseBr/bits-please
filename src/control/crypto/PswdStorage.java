97a79f75ccba7523461a7b2d75a23f17339e3ad2
digodk <diogodklein@gmail.com>
2018-07-06 19:46:08 -0300

    Alteração do packages
    Pasta do controller renomeada para control, adicionada a pasta crypto
    para funções critpográfica

---
+++ src/control/crypto/PswdStorage.java
@@ -0,0 +1,9 @@
+package control.crypto;
+
+public class PswdStorage {
+  public String clientPswdHash(String pswd, String email) {
+    final String salt = email + "bits-please";
+    String hash = "";
+    return hash;
+  }
+}
--- src/controller/PswdStorage.java
+++
@@ -1,9 +0,0 @@
-package controller;
-
-public class PswdStorage {
-  public String clientPswdHash(String pswd, String nome) {
-    String hash;
-    hash = "";
-    return hash;
-  }
-}
