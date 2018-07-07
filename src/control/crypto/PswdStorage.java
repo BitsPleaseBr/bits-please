de4eb1393b98ed373df215f7bac5243dc886fd7e
digodk <diogodklein@gmail.com>
2018-07-06 21:57:17 -0300

    Adicionada classe de hash BCrypt
    PswdStorage alterado para fazer uso do BCrypt

--- src/control/crypto/PswdStorage.java
+++ src/control/crypto/PswdStorage.java
@@ -1,9 +1,12 @@
 package control.crypto;
 
 public class PswdStorage {
-  public String clientPswdHash(String pswd, String email) {
-    final String salt = email + "bits-please";
-    String hash = "";
+  public static String clientPswdHash(String password, String email) {
+    final String prefixo = "$2a$10$";
+    String salt = email + "bitsplease";
+    salt = salt.replaceAll("[^A-Za-z0-9/.]", "a");
+    salt = prefixo + salt;
+    String hash = BCrypt.hashpw(password, salt);
     return hash;
   }
 }
