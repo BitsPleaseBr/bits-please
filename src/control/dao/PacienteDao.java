aeb2250bbbce14c4642ff9363beaa1e9e8485046
digodk <diogodklein@gmail.com>
2018-07-09 21:38:59 -0300

    Configuração final do PacienteDao

--- src/control/dao/PacienteDao.java
+++ src/control/dao/PacienteDao.java
@@ -1,5 +1,6 @@
 package control.dao;
 
+import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 
@@ -19,20 +20,21 @@ public class PacienteDao {
 	}
 	
 	//Método para efetuar o pré cadastro que permite o acesso de usuário no sistema
-	public void pre_cadastrar(PacienteBean obj) {
+	public void pre_cadastrar(PacienteBean paciente) {
 		
-		String sql = "INSERT INTO usuarios(nome, sobrenome, dataNasc, cpf, telefone, email, senha) VALUES(?, ?, ?, ?, ?, ?, ?)";
+		String sql = "INSERT INTO usuarios(nomeUser, sobrenomeUser, nascimentoUser, cpfUser, telefoneUser, emailUser, pswdUSer) VALUES(?, ?, ?, ?, ?, ?, ?)";
 		
 		//Tentar realizar o pré-cadastro
 		try{
 			PreparedStatement pstmt = conexao.prepareStatement(sql);
-			pstmt.setString(1, obj.getNome());
-			pstmt.setString(2, obj.getSobrenome());
-			pstmt.setDate(3, obj.getDataNasc());
-			pstmt.setInt(4, obj.getCpf());
-			pstmt.setInt(5, obj.getTelefone());
-			pstmt.setString(6, obj.getEmail());
-			pstmt.setString(7, obj.getSenha());
+			pstmt.setString(1, paciente.getNome());
+			pstmt.setString(2, paciente.getSobrenome());
+			pstmt.setDate(3, paciente.getDataNasc());
+			pstmt.setInt(4, paciente.getCpf());
+			pstmt.setInt(5, paciente.getTelefone());
+			pstmt.setString(6, paciente.getEmail());
+			// Converte a string de hash de senha em um binário para ser armazenado no bando de dados
+			pstmt.setBytes(7, paciente.getSenha().getBytes(StandardCharsets.UTF_8));
 			
 			pstmt.execute();
 			pstmt.close();
