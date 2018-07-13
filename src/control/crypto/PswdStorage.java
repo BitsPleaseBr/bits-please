f7d1fb0669544cd6957a5d77521474b4a9d1c41d
digodk <diogodklein@gmail.com>
2018-07-13 19:18:33 -0300

    Método para comparar hashes
    Criado método para comparar senha do login com hash obtido do servidor

--- src/control/crypto/PswdStorage.java
+++ src/control/crypto/PswdStorage.java
@@ -1,5 +1,7 @@
 package control.crypto;
 
+import java.nio.charset.StandardCharsets;
+
 /**
  * Classe para armazenamento de password
  * 
@@ -20,14 +22,20 @@ public class PswdStorage {
     final String prefixo = "$2a$10$"; // String prefixado no salt com parâmetros
                                       // de hashing para o algoritmo
     String salt = email + "bitspleasebrsaltcombr"; // O salt recebe um sufixo
-                                                   // para garantir que o salt
-                                                   // tenha um tamanho mínimo
-                                                   // necessário ]
+                                                   // para garantir um tamanho
+                                                   // mínimo
+                                                   // necessário 
+    
     // É necessário remover qualquer caractere que não estejam em Base64.
     salt = removerCaracteresInvalidos(salt);
     salt = prefixo + salt;
     String hash = BCrypt.hashpw(password, salt);
     return hash;
+  }
+  
+  public static boolean compararHashClient(String senha, byte[] hash) {
+    String hashString = new String(hash, StandardCharsets.UTF_8);
+    return BCrypt.checkpw(senha, hashString);
   }
 
   private static String removerCaracteresInvalidos(String salt) {
