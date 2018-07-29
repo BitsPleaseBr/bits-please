ec799c264645a3214790e62430e2f98eb36b2abc
digodk <diogodklein@gmail.com>
2018-07-28 23:50:18 -0300

    Removido erro da classe de testes de beans

--- testes/control/BeanTests.java
+++ testes/control/BeanTests.java
@@ -5,9 +5,7 @@ import model.bean.PacienteBean;
 public class BeanTests {
 
   public static void main(String[] args) {
-    PacienteBean paciente = new PacienteBean();
-    paciente.setCpf("057.575.29-40");
-    System.out.println(paciente.getCpf());
+    
 
   }
 
