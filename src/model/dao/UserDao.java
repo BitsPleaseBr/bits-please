cd8fd9e4c19f208dfb980c1dc8ff96da240cd7c4
104977 <104977@SALA415.proway.treina>
2018-07-30 20:19:29 -0300

    a

--- src/model/dao/UserDao.java
+++ src/model/dao/UserDao.java
@@ -4,6 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import model.bean.UserBean;
+import model.conexao.ConnectionFactory;
 
 public abstract class UserDao extends Dao {
 
@@ -32,5 +33,30 @@ public abstract class UserDao extends Dao {
     }
 
     return -1;
+  }
+
+  public static int login(String email, String senha) {
+	  
+	  String select = "select from TB_Users idUser where emailUser = ? and senhaUser = ?";
+	  
+	  try {
+		  
+		  PreparedStatement ps = ConnectionFactory.getConnection().prepareStatement(select);
+		  
+		  ps.setString(1, email);
+		  ps.setString(2, senha);
+		  
+		  ResultSet rs = ps.executeQuery();
+		  
+		  while(rs.next())
+			  return rs.getInt(1);
+		  
+	  } catch (SQLException e) {
+		  
+		  System.out.println("Não foi possível logar o usuário usando o email " + email + " e a senha " + senha);
+		  e.printStackTrace();
+	  }
+	  
+	  return -1;
   }
 }
