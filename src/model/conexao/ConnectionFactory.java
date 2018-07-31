a50a986b86f67539b072e62e49cb63fb1e5179f0
HillGus <39138506+HillGus@users.noreply.github.com>
2018-07-31 19:04:16 -0300

    removendo credenciais do BD do source code

--- src/model/conexao/ConnectionFactory.java
+++ src/model/conexao/ConnectionFactory.java
@@ -1,64 +1,73 @@
 package model.conexao;
 
-import java.io.IOException;
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
-import control.adicionais.Arquivo;
+import control.extras.Arquivo;
 
 /**
- * @author Diogo Classe para criar uma conexão com o banco de dados. Não pode
- *         ser instanciada.
+ * @author Diogo Classe para criar uma conexão com o banco de dados. Não pode ser instanciada.
  */
 public class ConnectionFactory {
-	// Constantes para acessar o banco de dados
-	private static String prefixoConnection = "";
-	private static String urlBD = "";
-	private static String porta = "";
-	private static String nomeBD = "";
-	private static String connectionURL = "";
-	private static String user = "";
-	private static String password = "";
+  
+  // Constantes para acessar o banco de dados
+  private static String prefixoConnection = "";
+  private static String urlBD = "";
+  private static String porta = "";
+  private static String nomeBD = "";
+  private static String connectionURL = "";
+  private static String user = "";
+  private static String password = "";
 
-	/**
-	 * Método para retornar a conexão com o banco de dados
-	 * 
-	 * @return Uma instância da conexão com o banco de dados se ela foi bem
-	 *         sucedida, caso contrário retorna null.
-	 */
-	public static Connection getConnection() {
+  static {
+    
+    String[] dados = new Arquivo().read("credenciais/conexoes.txt").split("##");
 
-		// Variável para retornar a conexão
-		Connection conexao = null;
+    prefixoConnection = dados[0];
+    urlBD = dados[1];
+    porta = dados[2];
+    nomeBD = dados[3];
+    connectionURL = prefixoConnection + urlBD + porta + nomeBD;
+    user = dados[4];
+    password = dados[5];
+  }
+  
+  /**
+   * Método para retornar a conexão com o banco de dados
+   * 
+   * @return Uma instância da conexão com o banco de dados se ela foi bem sucedida, caso contrário
+   *         retorna null.
+   */
+  public static Connection getConnection() {
 
-		// Realizar a conexão
-		try {
-			conexao = DriverManager.getConnection(connectionURL, user, password);
-		} catch (SQLException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
-		return conexao;
-	}
+    // Variável para retornar a conexão
+    Connection conexao = null;
 
-	private ConnectionFactory() {
+    // Realizar a conexão
+    try {
+      conexao = DriverManager.getConnection(connectionURL, user, password);
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw new RuntimeException(e);
+    }
+    return conexao;
+  }
+  
+  public static String getLocation() {
+    
+    String nomeClasse = "ConnectionFactory.class";
+    String path = new File(nomeClasse).getPath();
+    
+    int lengthNome = nomeClasse.length();
+    
+    System.out.println(path);
+    System.out.println(path.substring(0, path.length() - lengthNome));
+    
+    return path.substring(0, path.length() - lengthNome);
+  }
 
-		String[] dados = new String[6];
-		
-		try {
-			dados = new Arquivo().read("conexoes.txt").split("##");
-		} catch (IOException e) {
-
-			e.printStackTrace();
-		}
-
-		prefixoConnection = dados[0];
-		urlBD = dados[1];
-		porta = dados[2];
-		nomeBD = dados[3];
-		connectionURL = prefixoConnection + urlBD + porta + nomeBD;
-		user = dados[4];
-		password = dados[5];
-	}
+  private ConnectionFactory() {
+    
+  }
 }
