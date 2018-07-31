279b1db31983e040c6c932a7b94b583591a3569f
104977 <104977@SALA415.proway.treina>
2018-07-31 19:01:50 -0300

    removendo credenciais do BD

---
+++ src/control/extras/Arquivo.java
@@ -0,0 +1,62 @@
+package control.extras;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+
+public class Arquivo {
+    
+    public void Salvar(String local, String msg, boolean add) {
+        try {
+            File file = new File(local);
+            FileWriter fw = new FileWriter(file, add);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
+            pw.println(msg);
+            pw.close();
+        } catch (IOException e) {
+            System.out.println("Não foi possível salvar o arquivo");
+        }
+    }
+    
+    public String read(String local) {
+        String text = "";
+        try {
+          
+            File file = new File(getPath() + "/" + local);
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            while (br.ready()) {
+                text += br.readLine();
+            }
+            br.close();
+        } catch (IOError e) {
+          
+            System.out.println("Não foi possível ler o arquivo");
+            e.printStackTrace();
+        } catch (IOException e) {
+          
+          System.out.println("Não foi possível ler o arquivo");
+          e.printStackTrace();
+        }
+        return text;
+    }
+    
+    public String getPath() {
+      
+      String nomeClasse = "Arquivo.class";
+      String path = getClass().getResource(nomeClasse).getPath();
+      int pathLength = path.length();
+      
+      String classePath = "/control/extras/Arquivo.class";
+      int classePathLength = classePath.length();
+      
+      return path.substring(0, pathLength - classePathLength);
+    }
+}
--- src/model/conexao/ConnectionFactory.java
+++ src/model/conexao/ConnectionFactory.java
@@ -1,22 +1,38 @@
 package model.conexao;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import control.extras.Arquivo;
 
 /**
  * @author Diogo Classe para criar uma conexão com o banco de dados. Não pode ser instanciada.
  */
 public class ConnectionFactory {
+  
   // Constantes para acessar o banco de dados
-  private static final String prefixoConnection = "jdbc:mysql://";
-  private static final String urlBD = "bits-please.c6g4ywjszepf.sa-east-1.rds.amazonaws.com";
-  private static final String porta = ":3306";
-  private static final String nomeBD = "/bitsplease";
-  private static final String connectionURL = prefixoConnection + urlBD + porta + nomeBD;
-  private static final String user = "bitsadmin";
-  private static final String password = "nullpointerexception123";
+  private static String prefixoConnection = "";
+  private static String urlBD = "";
+  private static String porta = "";
+  private static String nomeBD = "";
+  private static String connectionURL = "";
+  private static String user = "";
+  private static String password = "";
 
+  static {
+    
+    String[] dados = new Arquivo().read("credenciais/conexoes.txt").split("##");
+
+    prefixoConnection = dados[0];
+    urlBD = dados[1];
+    porta = dados[2];
+    nomeBD = dados[3];
+    connectionURL = prefixoConnection + urlBD + porta + nomeBD;
+    user = dados[4];
+    password = dados[5];
+  }
+  
   /**
    * Método para retornar a conexão com o banco de dados
    * 
@@ -37,8 +53,21 @@ public class ConnectionFactory {
     }
     return conexao;
   }
+  
+  public static String getLocation() {
+    
+    String nomeClasse = "ConnectionFactory.class";
+    String path = new File(nomeClasse).getPath();
+    
+    int lengthNome = nomeClasse.length();
+    
+    System.out.println(path);
+    System.out.println(path.substring(0, path.length() - lengthNome));
+    
+    return path.substring(0, path.length() - lengthNome);
+  }
 
   private ConnectionFactory() {
-
+    
   }
 }
