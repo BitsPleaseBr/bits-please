62ae83a6ea9791fbe69a5551ef9296ee18e8285d
HillGus <guhillesheim@gmail.com>
2018-07-31 13:39:26 -0300

    removendo credenciais do servidor da source

---
+++ src/control/adicionais/Arquivo.java
@@ -0,0 +1,43 @@
+package control.adicionais;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+
+public class Arquivo {
+    
+    public void Salvar(String local, String msg, boolean add) throws IOException {
+        try {
+            File file = new File(local);
+            FileWriter fw = new FileWriter(file, add);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
+            pw.println(msg);
+            pw.close();
+        } catch (IOException e) {
+            System.out.println("Não foi possível salvar o arquivo");
+        }
+    }
+    
+    public String read(String local) throws FileNotFoundException, IOException {
+        String text = "";
+        try {
+            File file = new File("MeninaDosTemperos\\" + local);
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            while (br.ready()) {
+                text += br.readLine();
+            }
+            br.close();
+        } catch (IOError e) {
+            System.out.println("Não foi possível ler o arquivo");
+        }
+        return text;
+    }
+}
--- src/model/conexao/ConnectionFactory.java
+++ src/model/conexao/ConnectionFactory.java
@@ -1,44 +1,64 @@
 package model.conexao;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import control.adicionais.Arquivo;
+
 /**
- * @author Diogo Classe para criar uma conexão com o banco de dados. Não pode ser instanciada.
+ * @author Diogo Classe para criar uma conexão com o banco de dados. Não pode
+ *         ser instanciada.
  */
 public class ConnectionFactory {
-  // Constantes para acessar o banco de dados
-  private static final String prefixoConnection = "jdbc:mysql://";
-  private static final String urlBD = "bits-please.c6g4ywjszepf.sa-east-1.rds.amazonaws.com";
-  private static final String porta = ":3306";
-  private static final String nomeBD = "/bitsplease";
-  private static final String connectionURL = prefixoConnection + urlBD + porta + nomeBD;
-  private static final String user = "bitsadmin";
-  private static final String password = "nullpointerexception123";
+	// Constantes para acessar o banco de dados
+	private static String prefixoConnection = "";
+	private static String urlBD = "";
+	private static String porta = "";
+	private static String nomeBD = "";
+	private static String connectionURL = "";
+	private static String user = "";
+	private static String password = "";
 
-  /**
-   * Método para retornar a conexão com o banco de dados
-   * 
-   * @return Uma instância da conexão com o banco de dados se ela foi bem sucedida, caso contrário
-   *         retorna null.
-   */
-  public static Connection getConnection() {
+	/**
+	 * Método para retornar a conexão com o banco de dados
+	 * 
+	 * @return Uma instância da conexão com o banco de dados se ela foi bem
+	 *         sucedida, caso contrário retorna null.
+	 */
+	public static Connection getConnection() {
 
-    // Variável para retornar a conexão
-    Connection conexao = null;
+		// Variável para retornar a conexão
+		Connection conexao = null;
 
-    // Realizar a conexão
-    try {
-      conexao = DriverManager.getConnection(connectionURL, user, password);
-    } catch (SQLException e) {
-      e.printStackTrace();
-      throw new RuntimeException(e);
-    }
-    return conexao;
-  }
+		// Realizar a conexão
+		try {
+			conexao = DriverManager.getConnection(connectionURL, user, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		return conexao;
+	}
 
-  private ConnectionFactory() {
+	private ConnectionFactory() {
 
-  }
+		String[] dados = new String[6];
+		
+		try {
+			dados = new Arquivo().read("conexoes.txt").split("##");
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+		prefixoConnection = dados[0];
+		urlBD = dados[1];
+		porta = dados[2];
+		nomeBD = dados[3];
+		connectionURL = prefixoConnection + urlBD + porta + nomeBD;
+		user = dados[4];
+		password = dados[5];
+	}
 }
